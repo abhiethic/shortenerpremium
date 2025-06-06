9c1b10fd0b3fba12e313bb7d5b6f7822080b8d9a
abhiethic <168088445+abhiethic@users.noreply.github.com>
2025-06-06 13:21:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -92,7 +92,7 @@ USER_REPLY_TEXT = "ʙᴀᴋᴋᴀ ! ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴍʏ ꜱᴇɴᴘ
 OWNER_TAG = os.environ.get("OWNER_TAG", "marc_sp3ctor")
 UPI_ID = os.environ.get("UPI_ID", "@marc_sp3ctor")
 QR_PIC = os.environ.get("QR_PIC", "https://telegra.ph/file/3e83c69804826b3cba066-16cffa90cd682570da.jpg")
-SCREENSHOT_URL = os.environ.get("SCREENSHOT_URL", f"t.me/ninjanomo")
+SCREENSHOT_URL = os.environ.get("SCREENSHOT_URL", f"t.me/marc_sp3ctor")
 #--------------------------------------------
 #Time and its price
 #15 Days
